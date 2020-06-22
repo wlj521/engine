17f855ee016eb56b82173f9fa73f4802eee667d5
wanglijun <wanglijun@theduapp.com>
2020-06-22 14:26:37 +0800

    初始化项目

--- src/main/java/com/wlj/engine/EngineApplication.java
+++ src/main/java/com/wlj/engine/EngineApplication.java
@@ -3,11 +3,16 @@ package com.wlj.engine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 引擎启动类
+ *
+ * @author wlj
+ */
 @SpringBootApplication
 public class EngineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EngineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EngineApplication.class, args);
+    }
 
 }
